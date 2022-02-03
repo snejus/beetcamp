a9eeafcc27cbf1d7d10e4bc37111fa9047318e0d
Šarūnas Nejus <snejus@protonmail.com>
2022-02-03 18:09:10 +0000

    updated jsons and added test_lib

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -1,4 +1,3 @@
-import itertools as it
 import json
 import os
 import re
@@ -6,22 +5,17 @@ from collections import defaultdict
 from functools import partial
 
 import pytest
-from beetsplug.bandcamp import BandcampPlugin
-from beetsplug.bandcamp._metaguru import Metaguru
 from rich.columns import Columns
 from rich.traceback import install
-from rich_tables.utils import (
-    border_panel,
-    box,
-    make_console,
-    make_difftext,
-    new_table,
-    simple_panel,
-    wrap,
-)
+from rich_tables.utils import border_panel, make_console, make_difftext, new_table, wrap
+
+from beetsplug.bandcamp import BandcampPlugin
+from beetsplug.bandcamp._metaguru import Metaguru
+
+pytestmark = pytest.mark.lib
 
 target_dir = "dev"
-compare_against = "v0.11.0"
+compare_against = "0.11.0"
 if not os.path.exists(target_dir):
     os.makedirs(target_dir)
 install(show_locals=True, extra_lines=8, width=int(os.environ.get("COLUMNS", 150)))
@@ -72,96 +66,59 @@ def do_key(table, key: str, before, after) -> None:
         table.add_row(wrap(key, "b"), difftext)
 
 
+def compare(old, new) -> bool:
+    new["albumartist"] = new.pop("artist", "")
+    every_new = [new, *(new.get("tracks") or [])]
+    old["albumartist"] = old.pop("artist", "")
+    every_old = [old, *(old.get("tracks") or [])]
+    album_name = new.get("album")
+    album_id = wrap(new.get("album_id"), "dim")
+    keys_excl = {"bandcamp_artist_id", "bandcamp_album_id", "art_url_id", "art_url"}
+    keys_excl.update(("tracks", "comments", "length"))
+    table = new_table()
+    for new, old in zip(every_new, every_old):
+        title = " - ".join([new.get("artist") or "", new.get("title") or ""])
+        title = wrap(album_name or title, "b")
+        for key in sorted(set(new.keys()).union(set(old.keys())) - keys_excl):
+            do_key(table, key, str(old.get(key)), str(new.get(key)))
+
+    if table.rows:
+        console.print(border_panel(table, title=title, subtitle=album_id))
+        pytest.fail(pytrace=False)
+
+
 def test_file(file, config):
     with open(os.path.join("tests/jsons", file)) as f:
         guru = Metaguru(f.read(), config)
 
-    try:
-        if "_track_" in file:
-            album = guru.singleton
-        else:
-            album = guru.album
-    except Exception:
-        console.print_exception(max_frames=2, show_locals=True)
-        pytest.fail()
+    if "_track_" in file:
+        new = guru.singleton
+    else:
+        new = guru.album
 
     target = os.path.join(target_dir, file)
-    json.dump(album, open(target, "w"), indent=2)
+    json.dump(new, open(target, "w"), indent=2)
 
-    every_new = [album, *(album.get("tracks") or [])]
-    old_album = json.load(open(os.path.join(compare_against, file)))
-    every_old = [old_album, *(old_album.get("tracks") or [])]
-
-    album_name = album.get("album")
-    album_id = album.get("album_id")
-    old_album["albumartist"] = old_album.pop("artist", "")
-    album["albumartist"] = album.pop("artist", "")
-
-    keys_excl = {"bandcamp_artist_id", "bandcamp_album_id", "art_url_id", "art_url"}
-    for new, old in zip(every_new, every_old):
-        title = album_name or " - ".join(
-            [new.get("artist") or "", new.get("title") or ""]
-        )
-        table = new_table()
-        for key in sorted(
-            filter(lambda x: x not in {"tracks", "comments"}, set(new.keys()) - keys_excl)
-        ):
-            do_key(table, key, old.get(key), new.get(key))
-
-        if len(table.rows):
-            console.log(border_panel(table, title=wrap(title, "b"), subtitle=album_id))
-            pytest.fail(pytrace=False)
+    old = json.load(open(os.path.join(compare_against, file)))
+    compare(old, new)
 
 
-# @pytest.mark.skip
 def test_all():
-    failed = []
     config = BandcampPlugin().config.flatten()
     for testfile in testfiles:
-        print(testfile)
-        json_in = open(os.path.join("tests/jsons", testfile)).read()
-        guru = Metaguru(json_in, config)
-        try:
-            if "_track_" in testfile:
-                album = guru.singleton
-            else:
-                album = guru.album
-        except Exception:
-            console.print_exception(show_locals=True)
-            break
-            # failed.append(str(exc))
+        guru = Metaguru(open(os.path.join("tests/jsons", testfile)).read(), config)
+        if "_track_" in testfile:
+            new = guru.singleton
+        else:
+            new = guru.album
 
         target = os.path.join(target_dir, testfile)
-        json.dump(album, open(target, "w"), indent=2)
-
-        album["albumartist"] = album.pop("artist", "")
-        every_new = [album, *(album.get("tracks") or [])]
+        json.dump(new, open(target, "w"), indent=2)
+        old = json.load(open(os.path.join(compare_against, testfile)))
         try:
-            old_album = json.load(open(os.path.join(compare_against, testfile)))
-        except OSError as exc:
-            failed.append(str(exc))
-            continue
-
-        old_album["albumartist"] = old_album.pop("artist", "")
-        every_old = [old_album, *(old_album.get("tracks") or [])]
-
-        album_name = album.get("album")
-        album_id = wrap(album.get("album_id"), "dim")
-        for new, old in zip(every_new, every_old):
-            title = wrap(
-                album_name
-                or " - ".join([new.get("artist") or "", new.get("title") or ""]),
-                "b",
-            )
-            table = new_table()
-            for key in sorted(set(new.keys()) - {"length", "tracks", "comments"}):
-                do_key(table, key, old.get(key), new.get(key))
-
-            if len(table.rows):
-                console.print(
-                    simple_panel(table, title=title, subtitle=album_id, box=box.ROUNDED)
-                )
-    btable = partial(new_table, border_style="white")
+            compare(old, new)
+        except:  # noqa
+            pass
 
     cols = []
     for field in set(diffs.keys()) - {"comments"}:
@@ -170,18 +127,10 @@ def test_all():
 
     console.print(Columns(cols, expand=True))
 
-    stats_table = btable("field", "#")
+    stats_table = new_table("field", "#", border_style="white")
     for field, count in sorted(stats_map.items(), key=lambda x: x[1], reverse=True):
         stats_table.add_row(field, str(count))
-    stats_table.add_row("total", str(len(testfiles)))
-    console.print(stats_table)
-    # console.print(failed)
-    pytest.fail()
-
-    # for item, track_info in pairs:
-    #     track_info["artist"] = track_info.get("artist") or cur_artist
-    #     tracks_table.add_row(*_make_track_diff(item, track_info))
-    # console.print(border_panel(tracks_table))
-
-    if failed:
-        pytest.fail("The following files have failed: {}".format(str(failed)))
+    if stats_table.rows:
+        stats_table.add_row("total", str(len(testfiles)))
+        console.print(stats_table)
+        pytest.fail()
