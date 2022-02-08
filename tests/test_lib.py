4d7cd94d2fcb7ef31b2bd7c92c6d860817c5aefe
Šarūnas Nejus <snejus@protonmail.com>
2022-02-08 04:19:26 +0000

    Ignore test_lib for now

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -15,7 +15,7 @@ from beetsplug.bandcamp._metaguru import Metaguru
 pytestmark = pytest.mark.lib
 
 target_dir = "dev"
-compare_against = "test"
+compare_against = "5ba8239"
 if not os.path.exists(target_dir):
     os.makedirs(target_dir)
 install(show_locals=True, extra_lines=8, width=int(os.environ.get("COLUMNS", 150)))
