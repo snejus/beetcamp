53f3d5e8f5b813a0deca93c620897f1a66cbef8c
Šarūnas Nejus <snejus@protonmail.com>
2022-12-15 06:08:13 +0000

    Update testing jsons

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -26,8 +26,8 @@ pytestmark = pytest.mark.lib
 
 BASE_DIR = "lib_tests"
 TEST_DIR = "dev"
-# REFERENCE_DIR = "c61fc8e"
-REFERENCE_DIR = "c61fc8e"
+# REFERENCE_DIR = "fc17ecb"
+REFERENCE_DIR = "fc17ecb"
 JSONS_DIR = "jsons"
 
 IGNORE_FIELDS = {
