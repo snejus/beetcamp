5c76dc81f942d143168a768dc941e3824ca86f63
Šarūnas Nejus <snejus@protonmail.com>
2024-05-07 13:12:00 +0100

    cmdline search: add -p / --page flag

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -6,28 +6,20 @@ import pytest
 from beetsplug.bandcamp import get_args
 
 
+# fmt: off
 @pytest.mark.parametrize(
-    ["cmdline", "args"],
+    "cmdline, args",
     [
-        (
-            ["https://bandcamp.com"],
-            {
-                "query": "",
-                "release_url": "https://bandcamp.com",
-                "search_type": "",
-                "index": None,
-            },
-        ),
-        (["hello"], {"query": "hello", "search_type": "", "index": None}),
-        (["hello", "-a"], {"query": "hello", "search_type": "a", "index": None}),
-        (["hello", "-t"], {"query": "hello", "search_type": "t", "index": None}),
-        (["hello", "-l"], {"query": "hello", "search_type": "b", "index": None}),
-        (
-            ["hello", "-l", "-o", "1"],
-            {"query": "hello", "search_type": "b", "index": 1},
-        ),
+        (["https://bandcamp.com"], {"query": "", "release_url": "https://bandcamp.com", "search_type": "", "index": None, "page": 1}),
+        (["hello"], {"query": "hello", "search_type": "", "index": None, "page": 1}),
+        (["hello", "-a"], {"query": "hello", "search_type": "a", "index": None, "page": 1}),
+        (["hello", "-t"], {"query": "hello", "search_type": "t", "index": None, "page": 1}),
+        (["hello", "-l"], {"query": "hello", "search_type": "b", "index": None, "page": 1}),
+        (["hello", "-l", "-o", "1"], {"query": "hello", "search_type": "b", "index": 1, "page": 1}),
+        (["hello", "-l", "-p", "2"], {"query": "hello", "search_type": "b", "index": None, "page": 2}),
     ],
 )
+# fmt: on
 def test_cmdline_flags(cmdline, args):
     sys.argv = ["beetcamp", *cmdline]
     assert vars(get_args()) == args
